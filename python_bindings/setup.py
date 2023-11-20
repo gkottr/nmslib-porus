f04d01ea223ba3a40d46f71ace9587e7026dfe63
Комолов Владимир <VYuKomolov@sberbank.ru>
2023-11-20 11:11:57 +0300

    Tumba-Umba

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -11,8 +11,8 @@ if sys.platform.startswith("win") and struct.calcsize("P") * 8 == 32:
     raise RuntimeError("Windows 32-bit is not supported.")
 
 dep_list = ['pybind11>=2.2.3', 'psutil']
-dep_list.append("numpy>=1.10.0,<1.17 ; python_version=='2.7'")
-dep_list.append("numpy>=1.10.0 ; python_version>='3.5'")
+# dep_list.append("numpy>=1.10.0,<1.17 ; python_version=='2.7'")
+dep_list.append("numpy>=1.26.1 ; python_version>='3.5'")
 
 py_version = tuple([int(v) for v in sys.version.split('.')[:2]])
 if py_version != (2, 7) and py_version < (3, 5):
